761f9b7af595293ebd1e2486b816366a116d8da0
Sebastian Marin <sebbamarin@gmail.com>
2022-06-30 03:40:56 -0300

    fix cnx db

--- cnx/coffee_list.php
+++ cnx/coffee_list.php
@@ -1,6 +1,7 @@
 <?php
 
-$cnx = mysqli_connect("localhost","root","","cursophp") or exit("Error en la conexion");
+// $cnx = mysqli_connect("localhost","root","","cursophp") or exit("Error en la conexion");
+$cnx = mysqli_connect("localhost", "c2061385_cursoph", "50vaDUsagi", "c2061385_cursoph") or exit("Error en la conexion");
 
 $query = mysqli_query($cnx,"SELECT * FROM coffee_list");
 
